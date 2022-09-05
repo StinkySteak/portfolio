95f5e3c072e532bc12e1741a2fbed6eb3d36cdab
StinkySteak <slimesteve17@gmail.com>
2022-09-05 21:51:01 +0700

    firebase analytics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import AboutMe from './Pages/AboutMe';
 import Footer from './Pages/Footer';
 import Main from './Pages/Main';
 import Project from './Pages/Project'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkHmcnom1AZYSbgKmAs08srj5DRLOSKk8",
+    authDomain: "portfolio-570f1.firebaseapp.com",
+    projectId: "portfolio-570f1",
+    storageBucket: "portfolio-570f1.appspot.com",
+    messagingSenderId: "876298032126",
+    appId: "1:876298032126:web:8d89724a7ec355a7e666af",
+    measurementId: "G-FQG10PPVJ2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
+
   return (
     <>
       <Main />
